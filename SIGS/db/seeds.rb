035394412cf9e9a9d3702a20269aacdb9e5a5a60
Indiara Duarte <indiara96d@gmail.com>
2018-06-05 17:41:54 -0400

    Fixing seed file

--- SIGS/db/seeds.rb
+++ SIGS/db/seeds.rb
@@ -23,7 +23,7 @@ course_4 = Course.find_or_create_by(code: '09', name: 'Artes Visuais', departmen
 puts "Creating Coordinator Users"
 user = User.create(name: 'Caio Filipe', email: 'coordenador@unb.br', cpf: '05012345678', registration: '1234567', active: 1, password: '123456')
 coordinator = Coordinator.find_or_create_by(user: user, course: course_2)
-user_2 = User.create(name: 'João Busche', email: 'coordenador@unb.br', cpf: '05044448888', registration: '1234544', active: 0, password: '123456')
+user_2 = User.create(name: 'João Busche', email: 'coordenador2@unb.br', cpf: '05044448888', registration: '1234544', active: 0, password: '123456')
 coordinator_2 = Coordinator.find_or_create_by(user: user_2, course: course_4)
 user_3 = User.create(name: 'Daniel Marques', email: 'coordenador3@unb.br', cpf: '05044348888', registration: '1234546', active: 1, password: '123456')
 coordinator_3 = Coordinator.find_or_create_by(user: user_3, course: course_4)
