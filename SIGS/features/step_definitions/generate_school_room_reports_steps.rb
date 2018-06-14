10261c18679ef616d93ccff6c5777b4bda32a34e
MatheusRich <matheusrichardt@gmail.com>
2018-06-14 11:29:11 -0300

    Fixing school room report tests

--- SIGS/features/step_definitions/generate_school_room_reports_steps.rb
+++ SIGS/features/step_definitions/generate_school_room_reports_steps.rb
@@ -2,26 +2,10 @@ And (/^click on link 'Turmas' in 'Relatório'$/) do
   first('.dropdown-menu').click_link('Turmas')
 end
 
-And (/^I fill in 'relatorio' with 'Todas'$/) do
-	choose('to_all')
+And (/^I fill in 'relatorio' with '([^"]*)'$/) do |option|
+	choose(option)
 end
 
-And (/^I fill in 'relatorio' with 'Alocadas'$/) do
-	choose('to_allocations')
-end
-
-And (/^I fill in 'relatorio' with 'Não Alocadas'$/) do
-	choose('to_not_allocations')
-end
-
-When (/^I press 'Gerar Relatório Todas as turmas' link$/) do
-  click_link('Gerar Relatório Todas as turmas')
-end
-
-When (/^I press 'Gerar Relatório de Turmas Alocadas' link$/) do
-  click_link('Gerar Relatório de Turmas Alocadas')
-end
-
-When (/^I press 'Gerar Relatório de turmas não Alocadas' link$/) do
-  click_link('Gerar Relatório de turmas não Alocadas')
+And (/^I press '([^"]*)' link$/) do |link|
+  click_link(link)
 end
