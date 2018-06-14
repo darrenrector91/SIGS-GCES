d0b38244a62d2843393942bc204be89dd55da975
MatheusRich <matheusrichardt@gmail.com>
2018-06-14 14:11:00 -0300

    Refactoring school room report js

--- SIGS/app/assets/javascripts/school_room_reports.js
+++ SIGS/app/assets/javascripts/school_room_reports.js
@@ -1,21 +1,18 @@
 $(document).ready(function () {
   $(".reports_allocation, .reports_no_all").hide();
 
-  $(".all_reports_school_room").click(function () {
+  $(".all_reports_school_room, .allocations_reports_school_room, .no_all_reports_school_room").click(function () {
     $(".reports_allocation").hide();
     $(".reports_no_all").hide();
-    $(".reports_all").show();
-  });
-
-  $(".allocations_reports_school_room").click(function () {
-    $(".reports_allocation").show();
     $(".reports_all").hide();
-    $(".reports_no_all").hide();
-  });
+    let clickedElement = $(this);
 
-  $(".no_all_reports_school_room").click(function () {
-    $(".reports_all").hide();
-    $(".reports_no_all").show();
-    $(".reports_allocation").hide();
+    if (clickedElement.hasClass('all_reports_school_room')) {
+      $(".reports_all").show();
+    } else if (clickedElement.hasClass('allocations_reports_school_room')) {
+      $(".reports_allocation").show();      
+    } else {
+      $(".reports_no_all").show();
+    }
   });
 });
