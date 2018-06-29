d4eb558e8d3eb4930f59baf51e915cec2fd2fb86
MatheusRich <matheusrichardt@gmail.com>
2018-06-29 14:47:38 -0300

    Fixing cucumber tests

--- SIGS/features/step_definitions/registration_request_steps.rb
+++ SIGS/features/step_definitions/registration_request_steps.rb
@@ -2,52 +2,12 @@ When (/^click on button 'Solicitar Cadastro'$/) do
 	click_link ('Solicitar Cadastro')
 end
 
-And (/^I fill in 'name' with 'gesiel freitas'$/) do
-	fill_in('user[name]', :with=> 'gesiel freitas')
+And (/^I fill in user '([^"]*)' with '([^"]*)'$/) do |field, value|
+	fill_in("user[#{field}]", :with=> value)
 end
 
-And (/^I fill in 'name' with 'Ana'$/) do
-	fill_in('user[name]', :with=> 'Ana')
-end
-
-And (/^I fill in 'cpf' with '02919147307'$/) do
-	fill_in('user[cpf]', :with=> '02919147307')
-end
-
-And (/^I fill in 'cpf' with '1234'$/) do
-	fill_in('user[cpf]', :with=> '1234')
-end
-
-And (/^I fill in 'registration' with '2244537'$/) do
-	fill_in('user[registration]', :with=> '2244537')
-end
-
-And (/^I fill in 'password-user' with '123456'$/) do
-	fill_in('user[password]', :with=> '123456')
-end
-
-And (/^I fill in 'confirm_password' with '123456'$/) do
-	fill_in('user[password_confirmation]', :with=> '123456')
-end
-
-And (/^I fill in 'email' with 'gesiel@unb.br'$/) do
-	fill_in('user[email]', :with=> 'gesiel@unb.br')
-end
-
-And (/^I fill in 'email' with ''$/) do
-	fill_in('user[email]', :with=> '')
-end
-
-And (/^I fill in 'type' with 'administrative_assistant'$/) do
-	choose('adm_as')
-end
-
-And (/^I fill in 'type' with 'coordinator'$/) do
-	choose('coord')
-end
-
-And (/^I fill in 'type' with 'deg'$/) do
-	choose('deg_user')
+And (/^I fill in user type with '([^"]*)'$/) do |value| 
+	choose(value)
 end
 
 When (/^I press 'Enviar' button$/) do
