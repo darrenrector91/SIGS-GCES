e05aeea5cb959fb686880bfd976c1b1a26fe8340
Kairon Velozo <kairon333@gmail.com>
2018-09-20 17:24:24 -0300

    Removing exception handler for development environment.

--- SIGS/config/application.rb
+++ SIGS/config/application.rb
@@ -15,9 +15,5 @@ module SIGS
     config.assets.enabled = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.active_record.time_zone_aware_types = [:datetime, :time]
-
-    # Makes exception_handler gem available in dev environment
-    config.exception_handler = { dev: true }
-
   end
 end
