5cd3d111982430712c66209882fa92c2fe69f81a
João Pedro Sconetto <sconetto.joao@gmail.com>
2018-10-03 16:37:28 -0300

    Fixing deploy error
    Adding full path on deploy script to prevent error
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- script/deploy.sh
+++ script/deploy.sh
@@ -10,7 +10,7 @@
 
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
 docker pull projetosigs/sigs-gces:latest
-docker-compose -f ../docker-compose-dev.yml pull --include-deps db
-docker-compose -f ../docker-compose-dev.yml up -d
+docker-compose -f /home/sigs/SIGS-GCES/docker-compose-dev.yml pull --include-deps db
+docker-compose -f /home/sigs/SIGS-GCES/docker-compose-dev.yml up -d
 docker logout
 rm -r /home/sigs/.docker/config.json
